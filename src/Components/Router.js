39f86b1e7c90dcfb9dbed732dc932cd1f25609ed
writepro4 <writepro4@naver.com>
2020-06-09 19:36:55 +0900

    3.3 스타일컴포넌트 다운/ 적용 Header 폴더 삭제 / css 컴포넌트화

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,17 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
+import Header from "Components/Header";
 import Search from "Routes/Search";
 
 export default () => (
     <Router>
         {/*Switch는 url이 겹치는걸 방지해준다*/}
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" component={TV} />
-            <Route path="/search" component={Search} />
-            {/*url이 일치하는 경우가 없을 경우 / 홈으로 리다이렉트 시킨다.*/}
-            <Redirect from="*" to="/" />
-        </Switch>
+        <>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" component={TV} />
+                <Route path="/search" component={Search} />
+                <Redirect from="*" to="/" />
+            </Switch>
+        </>
     </Router>
 );
