587b33824ae136576c0036c27a1c719135a3729b
writepro4 <writepro4@naver.com>
2020-06-10 12:18:13 +0900

    3.8 url String 변환 작업 / id값 넘기기

--- src/api.js
+++ src/api.js
@@ -11,11 +11,39 @@ const api = axios.create({
 export const moviesApi = {
     nowPlaying: () => api.get("movie/now_playing"),
     upcoming: () => api.get("movie/upcoming"),
-    popular: () => api.get("movie/popular")
+    popular: () => api.get("movie/popular"),
+    movieDetail: id =>
+        api.get(`movie/${id}`, {
+            params: {
+                //api에서 필요한 매개변수 값
+                append_to_response: "videos"
+            }
+        }),
+    search: term =>
+        api.get("search/movie", {
+            params: {
+                //url string으로 변환하기 위해 하는 작업 encodeURIComponent
+                query: encodeURIComponent(term)
+            }
+        })
 };
 
 export const tvApi = {
     topRated: () => api.get("tv/top_rated"),
     popular: () => api.get("tv/popular"),
-    airingToday: () => api.get("tv/airing_today")
+    airingToday: () => api.get("tv/airing_today"),
+    showDetail: id =>
+        api.get(`tv/${id}`, {
+            params: {
+                //api에서 필요한 매개변수 값
+                append_to_response: "videos"
+            }
+        }),
+    search: term =>
+        api.get("search/tv", {
+            params: {
+                //url string으로 변환하기 위해 하는 작업 encodeURIComponent
+                query: encodeURIComponent(term)
+            }
+        })
 };
