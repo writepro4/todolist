ee960541b4572f5c2d228c02ed5c564335536373
writepro4 <writepro4@naver.com>
2020-06-18 15:24:38 +0900

    6.1 HomePresenter and Section Components

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import {createGlobalStyle} from "styled-components";
 import reset from "styled-reset";
 
 const globalStyles = createGlobalStyle`
@@ -15,7 +15,7 @@ const globalStyles = createGlobalStyle`
         font-size:12px;
         background-color:rgba(20, 20, 20, 1);
         color:white;
-        padding-top:50px;
+        padding-top:80px;
     }
 `;
 
