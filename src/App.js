7b7dfbd3d6fe2ee7e70b13a174170eb112c2d00a
laon.dev2 <laon.dev2@gmail.com>
2020-07-14 10:55:55 +0900

    [update] input 값을 state로 받아서 TodoList에 넣고, 그 안에서 TodoItem으로 처리해주기

--- src/App.js
+++ src/App.js
@@ -22,11 +22,53 @@ class App extends Component {
         return ++this.id;
     }
 
+    //handleChange는 input에 들어가는 값이 변했을 떄를 말하는데
+    handleChange = (e) =>{
+        //value값은 이벤트가 일어나는 값의 target으로 변수 설정
+        const {value} = e.target;
+        //따라서 state를 관리하는데 이 때 input의 state에 들어가는 값은
+        //위에서 설정해둔 value값
+        this.setState({
+            input : value
+        })
+    }
+
+    //그러면 이제 input에 넣은 것을 todos라는 배열에 넣어둬야함.
+    handleInsert = () => {
+        //미리 레퍼런스를 설정해두고, 앞으로 todos나 input은
+        //this.state를 앞에 안쓰고도 사용이 가능하다.
+        const {todos, input} = this.state;
+
+        //newTodo라는 변수는, 새로운 객체를 만들어주는것.
+        //handleInsert가 불러오면 바로 생성됨.
+        const newTodo = {
+            text:input,
+            done:false,
+            id:this.getId()
+        }
+
+        //newTodo로 새로운 객체가 만들어졌다면,
+        //setState로 todos안에 새로운 요소를 만들어서 넣어줌.
+        //이 때 todos는 ...todos즉 기존의 값에다가 newTodo를 불러와서 위에서
+        //설정해준 값대로 배열에 들어감.
+        //그리고나서 이제 input값을 초기화해준다.
+        this.setState({
+            todos : [...todos, newTodo],
+            input: ''
+        });
+    }
+
     render() {
+        //마찬가지로 레퍼런스를 미리 설정해둠
+        const {input, todos} = this.state;
+        const {
+            handleChange,
+            handleInsert
+        } =this;
         return (
             <PageTemplate>
-                <TodoInput/>
-                <TodoList/>
+                <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
+                <TodoList todos={todos}/>
             </PageTemplate>
         )
     }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,29 @@ import TodoItem from './TodoItem';
 
 class TodoList extends Component {
     render() {
+        //App에서 정의한 todos를 가져오는데,
+        const {todos} = this.props;
+
+        //todoList라는 메서드를 만들어준다.
+        //이는 todos라는 배열을 map함수로 하나씩 훓는데,
+        const todoList = todos.map(
+            //todo는
+            todo => (
+                //TodoItem의 값을 설정해준다. 이 때 key는 배열의 id값
+                //done의 유무도 todos의 done으로 설정해두고,
+                //todos.text를 변수로 보여주게 코드를 작성
+                <TodoItem
+                    key = {todo.id}
+                    done = {todo.done}>
+                    {todo.text}
+                </TodoItem>
+            )
+        )
+
+
         return(
             <div>
-                <TodoItem done>리액트 공부</TodoItem>
-                <TodoItem>아직 안함</TodoItem>
+                {todoList}
             </div>
         )
     }
